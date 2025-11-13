16b1abb2001ffc860820a801368ffd7303516cd8
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-13 02:13:40 +0000

    auto-commit: 2025m13-021340

--- .profile/auto-commit-all.sh
+++ .profile/auto-commit-all.sh
@@ -58,7 +58,7 @@ if [ $# -eq 0 ]; then
     if [ -n "$EXISTING_PIDS" ]; then
         for PID in $EXISTING_PIDS; do
             vecho " - Stopping old watcher (PID: $PID)..."
-            kill "$PID" >& 
+            kill "$PID" >& /dev/null
         done
         vecho "Waiting for old processes to release locks..."
         sleep 1
